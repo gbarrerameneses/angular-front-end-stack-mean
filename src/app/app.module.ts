b6168f5f91298334b7bc06331a011b85536b7f91
Gustavo Barrera <gustavo.8805@gmail.com>
2024-07-03 15:02:10 -0600

    creando objeto producto para enviar los datos, agregando routing una vez enviado el form & alerta toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 // Componentes
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Producto } from 'src/app/models/producto';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-crear-producto',
@@ -10,7 +13,11 @@ export class CrearProductoComponent implements OnInit {
   productoForm: FormGroup;
 
   // productoForm se iniciializa en el constructor
-  constructor(private fb: FormBuilder) { // utilizamos la clase FormBuilder que permite manejar de una buena manera los formularios
+  constructor(
+    private fb: FormBuilder, // utilizamos la clase FormBuilder que permite manejar de una buena manera los formularios
+    private router: Router, // redireccionar al usuraio a oro componente
+    private toastr: ToastrService
+  ) {
     this.productoForm = this.fb.group({ // pasamos el objeto de los elementos que tenemos en el formulario como parámetro
       producto: ['', Validators.required],
       categoria: ['', Validators.required],
@@ -23,6 +30,15 @@ export class CrearProductoComponent implements OnInit {
   }
 
   agregarProducto(){
-    console.log('prueba-->', this.productoForm);
+  // console.log('prueba-->', this.productoForm.get('producto')?.value); # accediendo al atributo
+    const PRODUCTO: Producto = {
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value,
+    }
+    console.log('producto -->',PRODUCTO);
+    this.toastr.success('¡El producto fue registrado con éxito!', 'Producto registrado...');
+    this.router.navigate(['/']) // navegamos a la ruta raíz
   }
 }
