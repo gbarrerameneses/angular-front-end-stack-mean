8d58471a6dec15d79b72822677a95a053989ca17
Gustavo Barrera <gustavo.8805@gmail.com>
2024-07-03 11:47:37 -0600

    agregando formulario reactivo & creando producto en consola

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-producto',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-producto.component.scss']
 })
 export class CrearProductoComponent implements OnInit {
+  productoForm: FormGroup;
 
-  constructor() { }
+  // productoForm se iniciializa en el constructor
+  constructor(private fb: FormBuilder) { // utilizamos la clase FormBuilder que permite manejar de una buena manera los formularios
+    this.productoForm = this.fb.group({ // pasamos el objeto de los elementos que tenemos en el formulario como parámetro
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  agregarProducto(){
+    console.log('prueba-->', this.productoForm);
+  }
 }
